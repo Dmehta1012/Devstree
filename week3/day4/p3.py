fabcfe2e921a1adc3a0631d62adec9ea8d06bee3
Dmehta1012 <devarsh.m@devstree.in>
2025-08-11 18:11:51 +0530

    Employee System

---
+++ week3/day4/p3.py
@@ -0,0 +1,49 @@
+class Employee:
+    def __init__(self, name, emp_id):
+        self.name = name
+        self.emp_id = emp_id
+
+    def calculate_salary(self):
+        pass
+
+    def display_info(self):
+        print(f"Name: {self.name}, ID: {self.emp_id}, Salary: ₹{self.calculate_salary()}")
+
+
+class FullTimeEmployee(Employee):
+    def __init__(self, name, emp_id, monthly_salary):
+        super().__init__(name, emp_id)
+        self.monthly_salary = monthly_salary
+
+    def calculate_salary(self):
+        return self.monthly_salary
+
+
+class PartTimeEmployee(Employee):
+    def __init__(self, name, emp_id, hours_worked, hourly_rate):
+        super().__init__(name, emp_id)
+        self.hours_worked = hours_worked
+        self.hourly_rate = hourly_rate
+
+    def calculate_salary(self):
+        return self.hours_worked * self.hourly_rate
+
+
+class Intern(Employee):
+    def __init__(self, name, emp_id, stipend):
+        super().__init__(name, emp_id)
+        self.stipend = stipend
+
+    def calculate_salary(self):
+        return self.stipend
+
+emp1 = FullTimeEmployee("Devarsh", 420, 42000)
+emp2 = PartTimeEmployee("Kuldeep", 105, 30,400)
+emp3 = Intern("Umesh", 103, 6000)
+
+employees = [emp1, emp2, emp3]
+
+
+for emp in employees:
+    emp.display_info()
+    
